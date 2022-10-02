bdcc6741717c60810df25fbfbd6ae1225248d760
purusanths <purusanth200@gmail.com>
2022-10-02 16:35:03 +0530

    basic data cleaning has been completed

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    ######################
+    # YOUR CODE HERE     #
+    ######################
+    logger.info("Downloading the data from W&B")
+    local_path = wandb.use_artifact("sample.csv:latest").file()
+    logger.info("Downloading ha been completed successfully")
+    logger.info("Reading the local file")
+    df = pd.read_csv(local_path)
+
+    logger.info("Basic pre-preocessing has been started")
+    idx =df['price'].between(args.min_price,args.max_price)
+    df= df[idx].copy()
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    logger.info("Basic preprocesing has been completed successfully")
+    logger.info("Stated to save the preprossed data")
+    df.to_csv("clean_sample.csv", index=False)
+    logger.info("Cleaned data has been saved successfully")
+
+    logger.info("output artifact is stated to upland to the W&B ")
+    artifact = wandb.Artifact(
+     args.output_artifact,
+     type=args.output_type,
+     description=args.output_description)
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
+
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,## INSERT TYPE HERE: str, float or int,
+        help="name of the imput artifact",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,## INSERT TYPE HERE: str, float or int,
+        help="name of the output artifact",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,## INSERT TYPE HERE: str, float or int,
+        help="type of the output artifact",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,## INSERT TYPE HERE: str, float or int,
+        help="Cleaned dataset",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,## INSERT TYPE HERE: str, float or int,
+        help="minimum fesible price",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,## INSERT TYPE HERE: str, float or int,
+        help="maximum fesible price",## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
